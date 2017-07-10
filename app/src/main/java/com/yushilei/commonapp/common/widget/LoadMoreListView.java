3f998bee199e446e72b04f924ce52183e8ee0dfe
shilei.yu <shilei.yu@zhaopin.com.cn>
2017-07-10 19:38:22 +0800

    1：准备实现 加载更多 RecyclerView

---
+++ app/src/main/java/com/yushilei/commonapp/common/widget/LoadMoreListView.java
@@ -0,0 +1,22 @@
+package com.yushilei.commonapp.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * 上拉加载更多 ListView
+ *
+ * @author shilei.yu
+ * @since on 2017/7/10.
+ */
+
+public class LoadMoreListView extends ListView {
+    public LoadMoreListView(Context context) {
+        super(context);
+    }
+
+    public LoadMoreListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/yushilei/commonapp/common/widget/LoadMoreRecyclerView.java
@@ -0,0 +1,26 @@
+package com.yushilei.commonapp.common.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * @author shilei.yu
+ * @since on 2017/7/10.
+ */
+
+public class LoadMoreRecyclerView extends RecyclerView {
+    public LoadMoreRecyclerView(Context context) {
+        super(context);
+    }
+
+    public LoadMoreRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+    }
+}
