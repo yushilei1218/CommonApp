6588bea3dd88cba553a2df0cf31812b5d332c837
yushilei <582072387@qq.com>
2017-08-13 23:57:16 +0800

    1： 测试多布局复用 RecyclerView 多holder实现

---
+++ app/src/main/java/com/yushilei/commonapp/common/adapter2/Factory.java
@@ -0,0 +1,13 @@
+package com.yushilei.commonapp.common.adapter2;
+
+/**
+ * @auther by yushilei.
+ * @time 2017/8/13-23:54
+ * @desc
+ */
+
+public class Factory {
+    public BaseHolder createHolder() {
+        return null;
+    }
+}
