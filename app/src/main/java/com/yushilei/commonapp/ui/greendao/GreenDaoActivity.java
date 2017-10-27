74d8ca8bf70506b220c37dd90fa1d86ec3630213
shilei.yu <shilei.yu@zhaopin.com.cn>
2017-10-27 13:03:34 +0800

    1，测试标记TAG

--- app/src/main/java/com/yushilei/commonapp/ui/greendao/GreenDaoActivity.java
+++ app/src/main/java/com/yushilei/commonapp/ui/greendao/GreenDaoActivity.java
@@ -34,6 +34,7 @@ public class GreenDaoActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        //1.1.0更新
         setOnClick(R.id.act_green_dao_query);
         setOnClick(R.id.act_green_dao_save);
         setOnClick(R.id.act_green_dao_clear);
