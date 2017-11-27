97e971263180647d678d5d2b6a1adc065562c4fa
shilei.yu <shilei.yu@zhaopin.com.cn>
2017-11-27 15:46:30 +0800

    1，测试Weex 设置图片Adapter

--- app/src/main/java/com/yushilei/commonapp/common/weex/ImageAdapter.java
+++ app/src/main/java/com/yushilei/commonapp/common/weex/ImageAdapter.java
@@ -1,12 +1,29 @@
 package com.yushilei.commonapp.common.weex;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
+import com.facebook.common.executors.CallerThreadExecutor;
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.DataSource;
+import com.facebook.datasource.DataSubscriber;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.orhanobut.logger.Logger;
+import com.taobao.weex.WXSDKManager;
 import com.taobao.weex.adapter.IWXImgLoaderAdapter;
 import com.taobao.weex.common.WXImageStrategy;
 import com.taobao.weex.dom.WXImageQuality;
 import com.yushilei.commonapp.R;
+import com.yushilei.commonapp.common.base.BaseApp;
 
 /**
  * @author shilei.yu
@@ -15,9 +32,40 @@ import com.yushilei.commonapp.R;
 
 public class ImageAdapter implements IWXImgLoaderAdapter {
     @Override
-    public void setImage(String url, ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
-        Logger.d(url);
-        view.setImageResource(R.mipmap.ic_head);
+    public void setImage(final String url, final ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
+        WXSDKManager.getInstance().postOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (view == null || view.getLayoutParams() == null) {
+                    return;
+                }
+                if (TextUtils.isEmpty(url)) {
+                    view.setImageBitmap(null);
+                    return;
+                }
+                if (view.getLayoutParams().width <= 0 || view.getLayoutParams().height <= 0) {
+                    return;
+                }
+                ImagePipeline imagePipeline = Fresco.getImagePipeline();
+                ImageRequest request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url))
+                        .setResizeOptions(new ResizeOptions(view.getLayoutParams().width, view.getLayoutParams().height))
+                        .setProgressiveRenderingEnabled(true).build();
+                DataSource<CloseableReference<CloseableImage>> dataSource = imagePipeline.fetchDecodedImage(request, BaseApp.AppContext);
+                dataSource.subscribe(new BaseBitmapDataSubscriber() {
+                    @Override
+                    protected void onNewResultImpl(Bitmap bitmap) {
+                        view.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    protected void onFailureImpl(DataSource<CloseableReference<CloseableImage>> dataSource) {
+                        dataSource.close();
+                    }
+                }, UiThreadImmediateExecutorService.getInstance());
+
+            }
+        }, 0);
+
     }
 
 }
