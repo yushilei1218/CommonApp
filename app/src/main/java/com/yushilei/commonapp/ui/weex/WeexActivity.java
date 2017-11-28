00e941034cfc00e87599bdacfb1ae2397b32eac0
shilei.yu <shilei.yu@zhaopin.com.cn>
2017-11-28 20:43:42 +0800

    1，WeexFragment 封装

--- app/src/main/java/com/yushilei/commonapp/ui/weex/WeexActivity.java
+++ app/src/main/java/com/yushilei/commonapp/ui/weex/WeexActivity.java
@@ -2,85 +2,16 @@ package com.yushilei.commonapp.ui.weex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import com.taobao.weex.IWXRenderListener;
-import com.taobao.weex.WXSDKInstance;
-import com.taobao.weex.common.WXRenderStrategy;
-import com.taobao.weex.utils.WXFileUtils;
 import com.yushilei.commonapp.R;
 
 public class WeexActivity extends AppCompatActivity {
-
-    private WXSDKInstance mWXSDKInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weex);
-
-        mWXSDKInstance = new WXSDKInstance(this);
-        mWXSDKInstance.registerRenderListener(new IWXRenderListener() {
-            @Override
-            public void onViewCreated(WXSDKInstance instance, View view) {
-                setContentView(view);
-            }
-
-            @Override
-            public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
-
-            }
-
-            @Override
-            public void onRefreshSuccess(WXSDKInstance instance, int width, int height) {
-
-            }
-
-            @Override
-            public void onException(WXSDKInstance instance, String errCode, String msg) {
-
-            }
-        });
-        /**
-         * WXSample 可以替换成自定义的字符串，针对埋点有效。
-         * template 是.we transform 后的 js文件。
-         * option 可以为空，或者通过option传入 js需要的参数。例如bundle js的地址等。
-         * jsonInitData 可以为空。
-         * width 为-1 默认全屏，可以自己定制。
-         * height =-1 默认全屏，可以自己定制。
-         */
-        mWXSDKInstance.render("WXSample", WXFileUtils.loadFileOrAsset("hello.js", this), null, null, WXRenderStrategy.APPEND_ASYNC);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance.onActivityResume();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance.onActivityPause();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance.onActivityStop();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mWXSDKInstance != null) {
-            mWXSDKInstance.onActivityDestroy();
-        }
+        WeexFragment fg = WeexFragment.newInstance(null, null);
+        getSupportFragmentManager().beginTransaction().add(R.id.act_weex, fg).commit();
     }
 }
---
+++ app/src/main/java/com/yushilei/commonapp/ui/weex/WeexFragment.java
@@ -0,0 +1,122 @@
+package com.yushilei.commonapp.ui.weex;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.taobao.weex.IWXRenderListener;
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.common.WXRenderStrategy;
+import com.taobao.weex.utils.WXFileUtils;
+import com.yushilei.commonapp.R;
+import com.yushilei.commonapp.common.base.BaseFragment;
+
+/**
+ * Weex Fragment基类
+ */
+public class WeexFragment extends BaseFragment {
+    private static final String WEEX_URL = "WEEX_URL";
+    private static final String H5_URL = "H5_URL";
+
+    private WXSDKInstance mWXSDKInstance;
+
+    private ViewGroup mContainer;
+
+    public WeexFragment() {
+    }
+
+    public static WeexFragment newInstance(String weexUrl, String h5Url) {
+        WeexFragment fg = new WeexFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(WEEX_URL, weexUrl);
+        bundle.putString(H5_URL, h5Url);
+        return fg;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_base_weex;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        showLoading();
+
+        mContainer = findView(R.id.fg_weex_container);
+
+        mWXSDKInstance = new WXSDKInstance(getActivity());
+        mWXSDKInstance.registerRenderListener(new IWXRenderListener() {
+            @Override
+            public void onViewCreated(WXSDKInstance instance, View view) {
+                if (mContainer != null) {
+                    hideLoading();
+                    mContainer.addView(view);
+                }
+            }
+
+            @Override
+            public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
+                hideLoading();
+            }
+
+            @Override
+            public void onRefreshSuccess(WXSDKInstance instance, int width, int height) {
+                hideLoading();
+            }
+
+            @Override
+            public void onException(WXSDKInstance instance, String errCode, String msg) {
+
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mWXSDKInstance.render("WXSample", WXFileUtils.loadFileOrAsset("hello.js", getActivity()), null, null, WXRenderStrategy.APPEND_ASYNC);
+            }
+        }, 1000);
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityPause();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityStop();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mWXSDKInstance != null) {
+            mWXSDKInstance.onActivityDestroy();
+        }
+    }
+}
