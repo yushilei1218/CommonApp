257499e1d76b1db58b88ed656f1512432a3067eb
shilei.yu <shilei.yu@zhaopin.com.cn>
2018-01-12 21:07:18 +0800

    1：测试RxJava线程变换

--- app/src/main/java/com/yushilei/commonapp/ui/rxjava/ProxyActivity.java
+++ app/src/main/java/com/yushilei/commonapp/ui/rxjava/ProxyActivity.java
@@ -1,6 +1,8 @@
 package com.yushilei.commonapp.ui.rxjava;
 
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.yushilei.commonapp.R;
@@ -16,19 +18,51 @@ public class ProxyActivity extends BaseActivity {
         return R.layout.activity_proxy;
     }
 
+    @SuppressWarnings("AlibabaAvoidManuallyCreateThread")
     @Override
     public void initView() {
-        new Observable<String, Integer>("1") {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-            @Override
-            public Integer apply(String data) {
-                return Integer.parseInt(data);
+                Observer<Integer> observer = new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Integer data) {
+                        log("onSubscribe =" + data.toString());
+                    }
+                };
+                new Observable<String, Integer>("1") {
+
+                    @Override
+                    public Integer apply(String data) {
+                        log("apply =" + data);
+                        return Integer.parseInt(data);
+                    }
+                }.subscribe(new ProxyObserver(observer));
             }
-        }.subscribe(new Observer<Integer>() {
-            @Override
-            public void onSubscribe(Integer data) {
-                Log.d(getTAG(), data.toString());
-            }
-        });
+        }).start();
+
+    }
+
+    private void log(String msg) {
+        Log.d(getTAG(), Thread.currentThread().getName() + " " + msg);
+    }
+
+    public static final class ProxyObserver<T> implements Observer<T> {
+        Observer<T> real;
+
+        public ProxyObserver(Observer<T> real) {
+            this.real = real;
+        }
+
+        @Override
+        public void onSubscribe(final T data) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    real.onSubscribe(data);
+                }
+            });
+        }
     }
 }
