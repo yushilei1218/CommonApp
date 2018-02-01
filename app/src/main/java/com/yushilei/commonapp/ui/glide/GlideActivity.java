3f5d5b9bb32784d91ff434100b32622a2cc79f26
shilei.yu <shilei.yu@zhaopin.com.cn>
2018-02-01 11:47:24 +0800

    1：添加Glide依赖库，跟着郭霖学习Glide

---
+++ app/src/main/java/com/yushilei/commonapp/ui/glide/GlideActivity.java
@@ -0,0 +1,15 @@
+package com.yushilei.commonapp.ui.glide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yushilei.commonapp.R;
+
+public class GlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide);
+    }
+}
